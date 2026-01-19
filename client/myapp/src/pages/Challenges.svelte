de2fc70eb708fcc37b7799571361975ea2b9f1d3
manuelWeb <devries.webdesigner@gmail.com>
2026-01-19 18:08:19 +0100

    feat: ADD basic pages/[Home,Games,GameDetail,Challenges,About,NotFound] wip

---
+++ client/myapp/src/pages/Challenges.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { onMount } from "svelte";
+
+    let challenges = [];
+    let loading = true;
+
+    onMount(async () => {
+        try {
+            const response = await fetch("http://api:3000/api/challenges");
+            if (!response.ok) throw new Error("Erreur lors du chargement");
+            challenges = await response.json();
+        } catch (error) {
+            console.error("Erreur:", error);
+        } finally {
+            loading = false;
+        }
+    });
+</script>
+
+<div class="h-full flex items-center justify-center">
+    <h1 class="text-6xl font-bold text-white mb-4">
+        Page liste des challenges
+    </h1>
+</div>
+
+<style>
+</style>
---
+++ client/myapp/src/pages/Games.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { onMount } from "svelte";
+
+    let games = [];
+    let loading = true;
+
+    onMount(async () => {
+        try {
+            const response = await fetch("http://api:3000/api/games");
+            if (!response.ok) throw new Error("Erreur lors du chargement");
+            games = await response.json();
+        } catch (error) {
+            console.error("Erreur:", error);
+        } finally {
+            loading = false;
+        }
+    });
+</script>
+
+<div class="h-full flex items-center justify-center">
+    <h1 class="text-6xl font-bold text-white mb-4">Page list de jeux</h1>
+</div>
+
+<style>
+</style>
