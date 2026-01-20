81068bcd50a8e0a9e4fc8981ba9a3b364a3a8aaa
BrahimElGhachi <belghachi@gmail.com>
2026-01-20 15:56:38 +0100

    feat: update of controllers base, challenge, game

--- api/controllers/challenge.controller.js
+++ api/controllers/challenge.controller.js
@@ -1,9 +1,17 @@
-import { user, Challenge } from '../models/index.js';
+import { Challenge, Game, User } from '../models/index.js';
 import BaseController from './base.controller.js';
 
 class ChallengeController extends BaseController {
     constructor() {
+        // On passe le modèle Challenge au BaseController
         super(Challenge, 'Challenge');
+    }
+
+    getRequestOptions(req) {
+        return [
+            { model: Game, as: 'game' },
+            { model: User, as: 'creator' }
+        ];
     }
 }
 
