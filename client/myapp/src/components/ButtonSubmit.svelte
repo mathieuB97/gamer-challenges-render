329ebb91a70f848fdf1400268b0ab46e654bf902
manuelWeb <devries.webdesigner@gmail.com>
2026-01-20 15:19:26 +0100

    feat: ADD ButtonSubmit and LabelInput components

---
+++ client/myapp/src/components/ButtonSubmit.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let text = "Add btn text";
+    export let className = "";
+</script>
+
+<button
+    type="submit"
+    class="w-full mt-3 py-3 rounded-lg bg-linear-to-r from-[#7b2cbf] to-[#00d9ff] hover:opacity-90 transition-opacity font-bold cursor-pointer {className}"
+>
+    {text}
+</button>
+
+<style>
+</style>
