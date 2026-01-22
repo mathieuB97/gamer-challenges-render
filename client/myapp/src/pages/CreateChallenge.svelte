b18aa2bb8ef8bc33f942ed1fc92c76a3d9a66e1f
manuelWeb <devries.webdesigner@gmail.com>
2026-01-22 20:11:58 +0100

    feat: front(client) page CreateChallenge add params on "retour" btn to go to the challenge game

--- client/myapp/src/pages/CreateChallenge.svelte
+++ client/myapp/src/pages/CreateChallenge.svelte
@@ -1,9 +1,11 @@
 <script>
     import AdviceBloc from "../components/AdviceBloc.svelte";
-
     import ButtonSubmit from "../components/ButtonSubmit.svelte";
     import Hero from "../components/Hero.svelte";
+    import IconArrowLeft from "../components/icon-arrow-left.svelte";
     import LabelInput from "../components/LabelInput.svelte";
+    import { routeParams } from "../router";
+
     let formData = {
         challengeName: "",
         challengeTitle: "",
@@ -39,25 +41,13 @@
         <div class="goback text-white/70">
             <a
                 class="flex gap-2 font-bold hover:text-white mb-6 transition-colors"
-                href="/challenges"
+                href={`/jeux/${$routeParams.gameId}/challenges`}
             >
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    class="lucide lucide-arrow-left w-5 h-5"
-                    ><path d="m12 19-7-7 7-7"></path><path d="M19 12H5"
-                    ></path></svg
-                >
+                <IconArrowLeft className="w-5 h-5" />
                 Retour
             </a>
         </div>
+
         <Hero
             title={{ default: "Créer un", highlight: "défi" }}
             paragraph="Lancez un nouveau challenge à la communauté"
