4e6f6cb41d368d9616e74ad38cdec26a35f54329
manuelWeb <devries.webdesigner@gmail.com>
2026-01-22 12:42:35 +0100

    feat: front(client) ButtonSubmit add variant [primary, secondary]

--- client/myapp/src/components/ButtonSubmit.svelte
+++ client/myapp/src/components/ButtonSubmit.svelte
@@ -1,14 +1,24 @@
 <script>
     export let text = "Add btn text";
     export let className = "";
+    export let variant = "primary";
 </script>
 
-<button
-    type="submit"
-    class="w-full mt-3 py-3 rounded-lg bg-linear-to-r from-[#7b2cbf] to-[#00d9ff] hover:opacity-90 transition-opacity font-bold cursor-pointer {className}"
->
-    {text}
-</button>
+{#if variant === "secondary"}
+    <button
+        type="submit"
+        class="w-full mt-3 py-3 rounded-lg border border-white/20 hover:bg-white/10 transition-colors cursor-pointer {className}"
+    >
+        {text}
+    </button>
+{:else}
+    <button
+        type="submit"
+        class="w-full mt-3 py-3 rounded-lg bg-linear-to-r from-[#7b2cbf] to-[#00d9ff] hover:opacity-90 transition-opacity font-bold cursor-pointer {className}"
+    >
+        {text}
+    </button>
+{/if}
 
 <style>
 </style>
