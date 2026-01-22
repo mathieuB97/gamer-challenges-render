f9866714491512c7acf91d6e6a6e4da997409550
BrahimElGhachi <belghachi@gmail.com>
2026-01-22 10:10:05 +0100

    feat: backend add challenge route and his controller

--- api/controllers/challenge.controller.js
+++ api/controllers/challenge.controller.js
@@ -1,4 +1,6 @@
-import { Challenge, Game, User } from '../models/index.js';
+// Importation des modèles nécessaires
+import { Challenge, Game, User, Contribution } from '../models/index.js';
+// Importation du contrôleur de base (modèle parent)
 import BaseController from './base.controller.js';
 
 class ChallengeController extends BaseController {
@@ -7,10 +9,32 @@ class ChallengeController extends BaseController {
         super(Challenge, 'Challenge');
     }
 
+    // Fonction pour définir les options de requête, notamment les jointures
     getRequestOptions(req) {
         return [
-            { model: Game, as: 'game' },
-            { model: User, as: 'creator' }
+            // Inclusion du modèle Game, alias 'game' dans les associations (récupère la totalité des infos de la table "game")
+            { model: Game, 
+                as: 'game' },
+
+            // Inclusion du modèle User, alias 'creator' dans les associations et "attributes" pour ne récupérer que le pseudo
+            { 
+                model: User, 
+                as: 'creator',
+                attributes: ['pseudo'], 
+            },
+
+            // Inclusion du modèle Contribution, alias 'contributions' avec une sous-inclusion du modèle User (l'auteur de la contribution)
+            { 
+                model: Contribution, 
+                as: 'contributions',
+                include: [
+                    {
+                        model: User,
+                        as: 'creator',
+                        attributes: ['pseudo'],
+                    }
+                ],
+            },
         ];
     }
 }
