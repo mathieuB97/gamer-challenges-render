7d243694a40b77ea2d11391203adfb430398a40b
manuelWeb <devries.webdesigner@gmail.com>
2026-01-22 13:37:23 +0100

    feat: front(client) ButtonSubmit add type to handle reset builtin html feat

--- client/myapp/src/components/ButtonSubmit.svelte
+++ client/myapp/src/components/ButtonSubmit.svelte
@@ -2,18 +2,20 @@
     export let text = "Add btn text";
     export let className = "";
     export let variant = "primary";
+    /** @type {'button' | 'submit' | 'reset'} */
+    export let type = "submit";
 </script>
 
 {#if variant === "secondary"}
     <button
-        type="submit"
+        {type}
         class="w-full mt-3 py-3 rounded-lg border border-white/20 hover:bg-white/10 transition-colors cursor-pointer {className}"
     >
         {text}
     </button>
 {:else}
     <button
-        type="submit"
+        {type}
         class="w-full mt-3 py-3 rounded-lg bg-linear-to-r from-[#7b2cbf] to-[#00d9ff] hover:opacity-90 transition-opacity font-bold cursor-pointer {className}"
     >
         {text}
