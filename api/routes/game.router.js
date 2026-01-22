ce2d1ccc6b4ec34e6868a6a3ddb3e80851e86319
BrahimElGhachi <belghachi@gmail.com>
2026-01-22 10:08:17 +0100

    feat: backend add game router

--- api/routes/game.router.js
+++ api/routes/game.router.js
@@ -3,7 +3,7 @@ import gameController from "../controllers/game.controller.js";
 
 const router = Router();
 
-router.get('/games', gameController.getAll);
-router.get('/games/:id', gameController.getById);
+router.get('/games', gameController.getAll); // liste de tous les jeux
+router.get('/games/:id', gameController.getById); // détails d'un jeu par son id 
 
 export default router;
