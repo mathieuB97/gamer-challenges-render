27d77cdc3f3cb60bdbe7e652f3bb30c186c2c7df
michaelsauviat <michael.sauviat@oclock.school>
2026-01-23 10:20:07 +0100

    feat:creating mock folders for front-end data

---
+++ client/myapp/src/mock/data.js
@@ -0,0 +1,153 @@
+export let topChallenges = [
+    {
+        id: 1,
+        title: "Nom du jeu",
+        challengeName: "Nom du défi",
+        image: "https://images.unsplash.com/photo-1757774636742-0a5dc7e5c07a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&w=600",
+        likes: 234,
+        participants: 120,
+    },
+    {
+        id: 2,
+        title: "Nom du jeu",
+        challengeName: "Nom du défi",
+        image: "https://images.unsplash.com/photo-1765430847336-596f709e8b3e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&w=600",
+        likes: 234,
+        participants: 120,
+    },
+    {
+        id: 3,
+        title: "Nom du jeu",
+        challengeName: "Nom du défi",
+        image: "https://images.unsplash.com/photo-1635372730136-06b29022281c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&w=600",
+        likes: 234,
+        participants: 120,
+    },
+    {
+        id: 7,
+        title: "Nom du jeu",
+        challengeName: "Nom du défi",
+        image: "https://images.unsplash.com/photo-1760604359369-45675611f750?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&w=600",
+        likes: 234,
+        participants: 120,
+    },
+
+];
+
+
+export let newChallenges = [
+    {
+        id: 7,
+        title: "Nom du jeu",
+        challengeName: "Nom du défi",
+        image: "https://images.unsplash.com/photo-1760604359369-45675611f750?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&w=600",
+        likes: 234,
+        participants: 120,
+    },
+    {
+        id: 8,
+        title: "Nom du jeu",
+        challengeName: "Nom du défi",
+        image: "https://images.unsplash.com/photo-1698300113381-6c5df7400fbf?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&w=600",
+        likes: 234,
+        participants: 120,
+    },
+    {
+        id: 9,
+        title: "Nom du jeu",
+        challengeName: "Nom du défi",
+        image: "https://images.unsplash.com/photo-1757774636742-0a5dc7e5c07a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&w=600",
+        likes: 234,
+        participants: 120,
+    },
+];
+
+export let leaderboardData = [
+    {
+        rank: 1,
+        name: "Nom du jeu",
+        pseudo: "Pseudo meilleur joueur",
+        image: "https://images.unsplash.com/photo-1757774636742-0a5dc7e5c07a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&w=400",
+    },
+    {
+        rank: 2,
+        name: "Nom du jeu",
+        pseudo: "Pseudo meilleur joueur",
+        image: "https://images.unsplash.com/photo-1765430847336-596f709e8b3e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&w=400",
+    },
+    {
+        rank: 3,
+        name: "Nom du jeu",
+        pseudo: "Pseudo meilleur joueur",
+        image: "https://images.unsplash.com/photo-1635372730136-06b29022281c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&w=400",
+    },
+    {
+        rank: 4,
+        name: "Nom du jeu",
+        pseudo: "Pseudo meilleur joueur",
+        image: "https://images.unsplash.com/photo-1759167625075-ee6173d53f8f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&w=400",
+    },
+    {
+        rank: 5,
+        name: "Nom du jeu",
+        pseudo: "Pseudo meilleur joueur",
+        image: "https://images.unsplash.com/photo-1765430847336-596f709e8b3e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&w=400",
+    },
+    {
+        rank: 6,
+        name: "Nom du jeu",
+        pseudo: "Pseudo meilleur joueur",
+        image: "https://images.unsplash.com/photo-1757774636742-0a5dc7e5c07a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&w=400",
+    },
+    {
+        rank: 7,
+        name: "Nom du jeu",
+        pseudo: "Pseudo meilleur joueur",
+        image: "https://images.unsplash.com/photo-1765430847336-596f709e8b3e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&w=400",
+    },
+    {
+        rank: 8,
+        name: "Nom du jeu",
+        pseudo: "Pseudo meilleur joueur",
+        image: "https://images.unsplash.com/photo-1635372730136-06b29022281c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&w=400",
+    },
+    {
+        rank: 9,
+        name: "Nom du jeu",
+        pseudo: "Pseudo meilleur joueur",
+        image: "https://images.unsplash.com/photo-1759167625075-ee6173d53f8f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&w=400",
+    },
+    {
+        rank: 10,
+        name: "Nom du jeu",
+        pseudo: "Pseudo meilleur joueur",
+        image: "https://images.unsplash.com/photo-1765430847336-596f709e8b3e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&w=400",
+    },
+];
+
+export let ongoingChallenges = [
+    {
+        id: 20,
+        title: "Nom du jeu",
+        challengeName: "Nom du défi",
+        image: "https://images.unsplash.com/photo-1759167625075-ee6173d53f8f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&w=400",
+        likes: 234,
+        participants: 120,
+    },
+    {
+        id: 2,
+        title: "Nom du jeu",
+        challengeName: "Nom du défi",
+        image: "https://images.unsplash.com/photo-1765430847336-596f709e8b3e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&w=600",
+        likes: 234,
+        participants: 120,
+    },
+    {
+        id: 9,
+        title: "Nom du jeu",
+        challengeName: "Nom du défi",
+        image: "https://images.unsplash.com/photo-1757774636742-0a5dc7e5c07a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&w=600",
+        likes: 234,
+        participants: 120,
+    },
+];
