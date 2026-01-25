811000de406aa7f92a21b53cfbb29e3a4850f5cc
Manu2015 <devries.webdesigner@gmail.com>
2026-01-25 18:10:20 +0100

    fix: front add game.service getGames(data to games list page)

--- client/myapp/src/lib/services/game.service.js
+++ client/myapp/src/lib/services/game.service.js
@@ -1,16 +1,9 @@
+import api from "../api.js";
 // Service pour récupérer la liste des jeux (games)
+
 export async function getGames() {
     try {
-        const response = await fetch("/api/games");
-        if (!response.ok) {
-            throw new Error("Erreur lors de la récupération des jeux");
-        }
-        const contentType = response.headers.get("content-type");
-        if (contentType && contentType.includes("application/json")) {
-            return await response.json();
-        } else {
-            throw new Error("Réponse non JSON reçue (probablement une page HTML)");
-        }
+        return await api("/games");
     } catch (error) {
         console.error("Erreur dans getGames:", error);
         return null;
