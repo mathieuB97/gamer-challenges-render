51805745ddd41da2981d689599013887a29a2f40
Manu2015 <devries.webdesigner@gmail.com>
2026-01-25 10:57:39 +0100

    feat: front ChallengeCard add icons like and participant(svg)

--- client/myapp/src/components/ChallengeCard.svelte
+++ client/myapp/src/components/ChallengeCard.svelte
@@ -1,11 +1,12 @@
 <script>
+  import IconLike from "./icon-like.svelte";
+  import IconParticipant from "./icon-participant.svelte";
   export let id;
   export let title;
   export let challengeName;
   export let image;
   export let likes;
   export let participants;
-
 
   function increment() {
     likes += 1;
@@ -18,8 +19,23 @@
     <h3 class="font-bold text-lg mb-1">{challengeName}</h3>
     <p class="text-sm text-gray-400 mb-2">{title}</p>
     <div class="flex items-center justify-between text-xs text-gray-400">
-      <span on:click={increment} style="cursor: pointer;">👍 {likes}</span>
-      <span>👥 {participants}</span>
+      <button
+        on:click={increment}
+        class="flex items-end gap-2 text-[#00D9FF] cursor-pointer"
+      >
+        <IconLike className="w-6 h-6" />
+        <span class="leading-3">
+          {likes}
+        </span>
+      </button>
+      <button
+        class="flex items-end gap-2 leading-3 text-[#00D9FF] cursor-pointer"
+      >
+        <IconParticipant />
+        <span class="leading-3">
+          {participants}
+        </span>
+      </button>
     </div>
   </div>
 </div>
