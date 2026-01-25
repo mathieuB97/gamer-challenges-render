f0f29297b41a4d5b5f0dbe3c0cc8ffccfc44b13f
BrahimElGhachi <belghachi@gmail.com>
2026-01-25 21:13:17 +0100

    ContributionController : ajout des jointures creator + challenge

--- api/controllers/contribution.controller.js
+++ api/controllers/contribution.controller.js
@@ -5,6 +5,26 @@ class ContributionController extends BaseController {
     constructor() {
         super(Contribution, 'Contribution');
     }
+
+    // Fonction pour définir les options de requête, notamment les jointures
+    getRequestOptions(req) {
+        return [
+            // Inclusion du modèle User, alias 'creator' dans les associations
+            // et "attributes" pour ne récupérer que le pseudo
+            { 
+                model: User, 
+                as: 'creator',
+                attributes: ['pseudo'], 
+            },
+
+            // Inclusion du modèle Challenge, alias 'challenge' dans les associations
+            { 
+                model: Challenge, 
+                as: 'challenge',
+                attributes: ['name', 'game_id'],
+            },
+        ];
     }
+}
 
 export default new ContributionController();
