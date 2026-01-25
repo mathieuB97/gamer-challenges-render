f786ed171097da3e1896465c67461249ccad0892
BrahimElGhachi <belghachi@gmail.com>
2026-01-25 21:18:07 +0100

    Routes : ajout authentification token sur route POST

--- api/routes/challenge.router.js
+++ api/routes/challenge.router.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import challengeController from "../controllers/challenge.controller.js";
-import { isAllowed } from "../middlewares/auth.middleware.js";
+import { isAllowed, validateToken } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.get('/challenges', challengeController.getChallengesGrouped); // Tous les challenges groupés par catégorie
 router.get('/leaderboard', challengeController.getLeaderboard); // Leaderboard des joueurs
 router.get('/challenges/:id', challengeController.getById); // Détails d'un challenge par son id
-router.post('/challenges', isAllowed('user'), challengeController.create); // Créer un nouveau challenge
+router.post('/challenges', validateToken, isAllowed('user'), challengeController.create); // Affiche de tous les challenges associés à un jeu
 
 export default router;
