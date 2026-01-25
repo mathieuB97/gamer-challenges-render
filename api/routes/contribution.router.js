1f124a818877b28dc5ec62163fea60b546fa3b6a
BrahimElGhachi <belghachi@gmail.com>
2026-01-25 21:18:34 +0100

    Routes : ajout authentification token sur route POST

--- api/routes/contribution.router.js
+++ api/routes/contribution.router.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import contributionController from "../controllers/contribution.controller.js";
+import { isAllowed, validateToken } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.post("/contributions", contributionController.create);
-
+router.post("/contributions", validateToken, isAllowed('user'), contributionController.create);
 export default router;
