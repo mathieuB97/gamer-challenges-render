9c864c5e03583a1dc4f54644d66bf382a5e0080f
manuelWeb <devries.webdesigner@gmail.com>
2026-01-27 18:30:08 +0100

    feat: front add service vote with getTopChallenges endpoint(votes/top-challenges)

--- client/myapp/src/lib/services/vote.service.js
+++ client/myapp/src/lib/services/vote.service.js
@@ -4,3 +4,8 @@ import api from '../api.js';
 export async function getChallengeVoteCount(challengeId) {
   return api(`/votes/challenge/${challengeId}`);
 }
+
+// Récupère les top challenges par nombre de votes
+export async function getTopChallenges() {
+  return api(`/votes/top-challenges`);
+}
